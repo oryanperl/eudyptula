5033f38e98835fe2fbbecef479102a010eb440e9
Oryan Perlmutter <oryan.perl1997@gmail.com>
2018-08-22 00:26:51 +0300

    added licensing to hello_world.c

--- 04-coding_style/hello_world.c
+++ 04-coding_style/hello_world.c
@@ -18,3 +18,7 @@ static void __exit hello_world_cleanup(void)
 
 module_init(hello_world_init);
 module_exit(hello_world_cleanup);
+
+MODULE_LICENSE("GPL");
+MODULE_AUTHOR("Oryan Perlmutter <oryan.perl1997@gmail.com>");
+MODULE_DESCRIPTION("Simple hello world kernel module");
