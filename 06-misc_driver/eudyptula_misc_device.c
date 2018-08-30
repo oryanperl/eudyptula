2a062548d38c92df0e1b60e71df4941403e87266
Oryan Perlmutter <oryan.perl1997@gmail.com>
2018-08-30 22:27:49 +0300

    Misc device ready

---
+++ 06-misc_driver/eudyptula_misc_device.c
@@ -0,0 +1,53 @@
+#include <linux/kernel.h>
+#include <linux/init.h>
+#include <linux/module.h>
+#include <linux/fs.h>
+#include <linux/stat.h>
+#include <linux/miscdevice.h>
+
+#include "eudyptula_misc_device.h"
+
+MODULE_LICENSE("GPL");
+MODULE_AUTHOR("Oryan Perlmutter");
+MODULE_DESCRIPTION("A simple miscellaneous driver which interacts with usermode");
+
+static ssize_t eudyptula_misc_read(struct file *filp, char *buffer,
+				   size_t len, loff_t *offset)
+{
+	return simple_read_from_buffer(buffer, len, offset, EUDYPTULA_ID "\n",
+			sizeof(EUDYPTULA_ID) + 1);
+}
+
+static ssize_t eudyptula_misc_write(struct file *filp, const char *buffer,
+				    size_t len, loff_t *offset)
+{
+	ssize_t ret = -1;
+	/* Allocate one extra byte to handle the case in which
+	 * EUDYPTULA_ID is a substring of the given buffer
+	 */
+	char id_buffer[sizeof(EUDYPTULA_ID) + 1] = {0};
+
+	ret = simple_write_to_buffer(id_buffer, sizeof(id_buffer), offset,
+				     buffer, len - 1);
+	if (ret < 0)
+		return ret;
+	if (strncmp(EUDYPTULA_ID, id_buffer, sizeof(EUDYPTULA_ID)) != 0)
+		return -EINVAL;
+
+	return len;
+}
+
+static const struct file_operations eudyptula_misc_fops = {
+	.owner = THIS_MODULE,
+	.read = eudyptula_misc_read,
+	.write = eudyptula_misc_write
+};
+
+static struct miscdevice eudyptula_misc_device = {
+	.minor = MISC_DYNAMIC_MINOR,
+	.name = MISC_DEVNAME,
+	.fops = &eudyptula_misc_fops,
+	.mode = S_IRWXUGO
+};
+
+module_misc_device(eudyptula_misc_device);
---
+++ 06-misc_driver/eudyptula_misc_device.h
@@ -0,0 +1,7 @@
+#ifndef __EUDYPTULA_MISC_DRIVER_H__
+#define __EUDYPTULA_MISC_DRIVER_H__
+
+#define EUDYPTULA_ID "0a02c8c3ca21"
+#define MISC_DEVNAME "eudyptula"
+
+#endif // __EUDYPTULA_MISC_DRIVER_H__
